3107a24ad7c85633d803bfcb746ab023c5dfb2a2
choufengleilei <choufengleilei@126.com>
2021-12-29 14:30:13 +0800

    映射工具库

--- mapping/config.go
+++ mapping/config.go
@@ -2,6 +2,7 @@ package mapping
 
 type AddressConfig struct {
 	MongoAddr string `default:"mongodb://127.0.0.1:27017"`
-	Database  string `default:"address"`
+	Database  string `default:"mapping"`
 	Col       string `default:"addr"`
+	GidAddr   string `default:"http://gid.mytokenpocket.vip"`
 }
--- mapping/mapping.go
+++ mapping/mapping.go
@@ -1,8 +1,11 @@
 package mapping
 
 import (
+	"errors"
 	log "github.com/cihub/seelog"
 	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"github.com/tokenbankteam/tb_common/gid"
 )
 
 type Item struct {
@@ -11,9 +14,10 @@ type Item struct {
 }
 
 type Model struct {
-	Session  *mgo.Session
-	Database *mgo.Database
-	Col      *mgo.Collection
+	Session   *mgo.Session
+	Database  *mgo.Database
+	Col       *mgo.Collection
+	GidServer *gid.Server
 }
 
 func NewMapping(config *AddressConfig) (*Model, error) {
@@ -24,34 +28,63 @@ func NewMapping(config *AddressConfig) (*Model, error) {
 	}
 	database := session.DB(config.Database)
 	col := database.C(config.Col)
-
+	gidServer := gid.NewServer(config.GidAddr)
 	return &Model{
-		Session:  session,
-		Database: database,
-		Col:      col,
+		Session:   session,
+		Database:  database,
+		Col:       col,
+		GidServer: gidServer,
 	}, nil
 }
 
 func (s *Model) GetUkByItem(n string) (*Item, error) {
-
-	return nil, nil
+	item := new(Item)
+	if err := s.Col.Find(bson.M{"n": n}).All(&item); err != nil {
+		if err == mgo.ErrNotFound {
+			result, err := s.GidServer.Get()
+			if err != nil {
+				return nil, err
+			}
+			if err = s.Col.Insert(bson.M{"n": n, "uk": result.Id}); err != nil {
+				return nil, err
+			}
+			return &Item{N: n, Uk: result.Id}, nil
+		}
+		log.Errorf("get uk by item n %v error %v", n, err)
+		return nil, err
+	}
+	return item, nil
 }
 
 func (s *Model) GetUkByItemList(ns []string) ([]*Item, error) {
-
-	return nil, nil
+	if len(ns) > 500 {
+		return nil, errors.New("params maximum limit exceeded")
+	}
+	list := make([]*Item, 0)
+	if err := s.Col.Find(bson.M{"n": bson.M{"$in": ns}}).All(&list); err != nil {
+		log.Errorf("get uk by item list %v error %v", ns, err)
+		return nil, err
+	}
+	return list, nil
 }
 
 func (s *Model) GetItemByUk(uk int64) (*Item, error) {
-
-	return nil, nil
+	item := new(Item)
+	if err := s.Col.Find(bson.M{"uk": uk}).All(&item); err != nil {
+		log.Errorf("get item by uk error %v", err)
+		return nil, err
+	}
+	return item, nil
 }
 
 func (s *Model) GetItemByUkList(uks []int64) ([]*Item, error) {
-
-	return nil, nil
-}
-
-func (s *Model) Insert(item *Item) {
-
+	if len(uks) > 500 {
+		return nil, errors.New("params maximum limit exceeded")
+	}
+	list := make([]*Item, 0)
+	if err := s.Col.Find(bson.M{"uk": bson.M{"$in": uks}}).All(&list); err != nil {
+		log.Errorf("get item by uk list %v error %v", uks, err)
+		return nil, err
+	}
+	return list, nil
 }
